005e96d9ea19ed0e7d5aa2644925fa068d28dcd1
Jacob <jelondon12@gmail.com>
2019-11-09 09:01:31 -0800

    example development

--- demo/first.c
+++ demo/first.c
@@ -14,25 +14,26 @@ static Window *window;
 static bool call_keys = true;
 static bool call_draw = true;
 
-float px = 8.0f;
-float py = 8.0f;
-float pa = 0.0f;
-float fov = 3.14159 / 3.0;
-float step = 3.0f;
+float px    = 8.0f;
+float py    = 8.0f;
+float pa    = 0.0f;
+float fov   = M_PI / 3.0f;
+float step  = 3.0f;
 float rot_r = 2.0f;
 float rot_l = -2.0f;
 #define map_width 16
 #define map_height 16
-char wall_char = '#';
-int depth = 16;
+int depth       = 16;
 float cast_step = 0.1f;
+char wall_char  = '#';
+char at_char    = '@';
 char map[map_width][map_height] = {
     "################",
     "#..............#",
     "#...........#..#",
     "#....##........#",
     "#..........##..#",
-    "#..............#",
+    "#..@...........#",
     "#..............#",
     "#......#.......#",
     "#......#.......#",
@@ -52,15 +53,15 @@ int walk_step = 2;
 
 static void move(int direction, int strafe, int step)
 {
-    px += direction * sinf(pa + strafe * 3.14159 / 2.0f) * step * 0.05;
-    py += direction * cosf(pa + strafe * 3.14159 / 2.0f) * step * 0.05;
+    px += direction * sinf(pa + strafe * M_PI / 2.0f) * step * 0.05f;
+    py += direction * cosf(pa + strafe * M_PI / 2.0f) * step * 0.05f;
 
     // collision detection
     if (map[(int)py][(int)px] == wall_char) {
         if (strafe != 0)
             direction = -1;
-        px -= direction * sinf(pa - strafe * 3.14159 / 2) * step * 0.05;
-        py -= direction * cosf(pa - strafe * 3.14159 / 2) * step * 0.05;
+        px -= direction * sinf(pa - strafe * M_PI / 2.0f) * step * 0.05f;
+        py -= direction * cosf(pa - strafe * M_PI / 2.0f) * step * 0.05f;
     }
 }
 
@@ -71,10 +72,10 @@ static void keys()
         ctx->window->quit = true;
     }
     if (ctx->keystate[SDL_SCANCODE_LEFT]) {
-        pa += rot_l * 0.035;
+        pa += rot_l * 0.035f;
     }
     if (ctx->keystate[SDL_SCANCODE_RIGHT]) {
-        pa += rot_r * 0.035;
+        pa += rot_r * 0.035f;
     }
     if (ctx->keystate[SDL_SCANCODE_W]) {
         move(1, 0, walk_step);
@@ -98,14 +99,15 @@ static void draw()
 
     // walls
     for (int x = 0; x < window->width; x++) {
-        float ray_angle = (pa - fov / 2) + (float)x / (float)window->width * fov;
-        float distance_to_wall = 0;
+        float ray_angle = (pa - fov / 2.0f) + (float)x / (float)window->width * fov;
+        float distance_to_wall = 0.0f;
         bool hit_wall = false;
+        bool hit_at   = false;
         
         float eye_x = sinf(ray_angle);
         float eye_y = cosf(ray_angle);
 
-        while (!hit_wall && distance_to_wall < depth) {
+        while (!hit_at && !hit_wall && distance_to_wall < depth) {
             distance_to_wall += cast_step;
             int test_x = px + eye_x * distance_to_wall;
             int test_y = py + eye_y * distance_to_wall;
@@ -118,19 +120,30 @@ static void draw()
             // ray is in bounds
             else {
                 // cell is a wall
-                if (map[test_y][test_x] == wall_char)
+                if (map[test_y][test_x] == at_char)
+                    hit_at = true;
+                // cell is an at
+                else if (map[test_y][test_x] == wall_char) {
                     hit_wall = true;
+                }
                 // cell is something else
                 else {
 
                 }
             }
         }
-        int ceiling = window->height / 2.0 - window->height / distance_to_wall;
+
+        int ceiling = (float)window->height / 2.0f - (float)window->height / distance_to_wall;
         int floor = window->height - ceiling;
-        unsigned char s = fmax(fmin((int)(1.0f / distance_to_wall * 255 * 3), 255), 0);
-        // draw walls
-        painter_rect_fill(window->rend, (SDL_Color){s, s, s, 255}, (SDL_Rect){x, ceiling, 1, floor - ceiling});
+        if (hit_at) {
+            painter_rect_fill(window->rend, RED, (SDL_Rect){x, ceiling, 1, floor - ceiling});
+        }
+        if (hit_wall) {
+            // grayscale
+            unsigned char s = fmax(fmin((int)(1.0f / distance_to_wall * 255 * 3), 255), 0);
+            // draw walls (current pixel column)
+            painter_line(window->rend, (SDL_Color){s, s, s, 255}, x, ceiling, x, floor);
+        }
     }
 }
 
